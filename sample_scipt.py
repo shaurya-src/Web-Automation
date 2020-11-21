47b2f5559735540fa7f8aad7bc389396cf82f6cd
Shaurya Choudhary <shaurya.src@gmail.com>
2020-11-21 12:14:54 +0530

    Initialize enhanceCV

---
+++ sample_scipt.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.action_chains import ActionChains
+
+opts = Options()
+opts.add_argument('--start-maximized')
+driver = webdriver.Chrome(executable_path=r"C:\chromedriver_win32\chromedriver.exe", options=opts)
