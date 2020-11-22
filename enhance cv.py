459d3089a1bb404523f9c80085e6c5f326454e7f
Shaurya Choudhary <shaurya.src@gmail.com>
2020-11-22 20:26:01 +0530

    Migrated to PyCharm

--- enhance cv.py
+++ enhance cv.py
@@ -5,3 +5,5 @@ from selenium.webdriver.common.action_chains import ActionChains
 opts = Options()
 opts.add_argument('--start-maximized')
 driver = webdriver.Chrome(executable_path=r"C:\chromedriver_win32\chromedriver.exe", options=opts)
+
+driver.get("https://www.google.co.in/")
